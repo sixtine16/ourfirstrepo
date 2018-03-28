4aa0445d24b260b0dba1e83f85a9b6724acbfbdf
cece9 <cece_92@hotmail.it>
2018-03-28 18:26:32 +0100

    JS appropriate for presentation

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ function getStylesheet() {
         $('#breakfast-button').show();
         console.log("breakfast time")
 
-      } else if (( 12 <= currentTime) && (currentTime < 17)) {
+      } else if (( 12 <= currentTime) && (currentTime < 19)) {
         $('#lunch').show();
         $('#lunch-button').show();
         console.log("lunch time")
